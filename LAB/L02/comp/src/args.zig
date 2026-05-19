bf59e2b3381649b12adcb5916e9cb1dee9d4b77a
Marinix <122127061+Marynxs@users.noreply.github.com>
2026-05-19 01:31:12 +0000

    Arrumando o comp para uma pasta normal

---
+++ LAB/L02/comp/src/args.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+
+pub const Config = struct {
+    help: bool = false,
+    verbose: bool = false,
+};
+
+pub fn configParser(args: []const [:0]const u8) !Config {
+    var config = Config{};
+
+    if (args.len == 1) {
+        config.help = true;
+        return config;
+    }
+
+    var i: usize = 1;
+
+    std.debug.print("# de argumentos: {d}\n", .{args.len});
+
+    while (i < args.len) : (i += 1) {
+        const arg = args[i];
+
+        std.debug.print("Arg [{d}] = {s}\n", .{ i, arg });
+
+        if (std.mem.startsWith(u8, arg, "-")) {
+            const option = arg[1];
+
+            std.debug.print("Option: {c}\n", .{option});
+
+            switch (option) {
+                'h' => config.help = true,
+                'v' => config.verbose = true,
+                else => {},
+            }
+        }
+    }
+
+    return config;
+}
---
+++ LAB/L02/comp/src/main.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+
+const cli = @import("args.zig");
+
+const token = @import("token.zig");
+const ast = @import("ast.zig");
+const parser = @import("parser.zig");
+const eval = @import("eval.zig");
+const codegen = @import("codegen.zig");
+
+pub fn main(init: std.process.Init) !void {
+    const gpa = init.gpa;
+
+    const args = try init.minimal.args.toSlice(gpa);
+
+    defer gpa.free(args);
+    const config = try cli.configParser(args);
+
+    if (config.help) {
+        showHelp();
+        std.process.exit(0);
+    }
+    else if (config.verbose) {
+        std.debug.print("Modo verboso ativado\n", .{});
+        std.process.exit(0);
+    }
+}
+
+pub fn showHelp() void {
+    const msg =
+        \\Uso: comp [opções] <arquivo>
+        \\
+        \\Opções:
+        \\  -h, --help     Mostra esta ajuda
+        \\ -v, --verbose  Ativa o modo verboso
+        \\
+    ;
+    std.debug.print("{s}\n", .{msg});
+}
+
+test "importações" {
+    std.testing.refAllDecls(@This());
+
+    _ = token;
+    _ = ast;
+    _ = parser;
+    _ = eval;
+}
